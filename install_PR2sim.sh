73f508793bab63287eeb1073a80cf07a46f33c17
David Trimoulet <dtrimoul@lannister.laas.fr>
2018-07-06 13:27:30 +0200

    ajout de robosherlock + oppenni

--- install_PR2sim.sh
+++ install_PR2sim.sh
@@ -1,37 +1,42 @@
 #!/bin/bassh
 
-echo "Installing dependancies"
-sudo apt-get install libfcl0.5 libfcl-0.5-dev scons libbson-dev libglew-dev libglew1.13 ros-kinetic-octomap ros-kinetic-octomap-mapping ros-kinetic-octomap-msgs ros-kinetic-octomap-ros ros-kinetic-octomap-rviz-plugins ros-kinetic-octomap-server ros-kinetic-geometric-shapes ros-kinetic-warehouse-ros ros-kinetic-srdfdom ros-kinetic-object-recognition-msgs ros-kinetic-navigation ros-kinetic-ivcon ros-kinetic-convex-decomposition python-pip
-
-sudo apt-get install ros-kinetic-pr2-common
-sudo apt-get install ros-kinetic-image-geometry
-sudo apt-get install ros-kinetic-cv-bridge
-sudo apt-get install ros-kinetic-perceptions-pcl
-sudo apt-get install ros-kinetic-gazebo-plugins
-sudo apt-get install ros-kinetic-ompl
-
-#gestion des contrler pour teleop
-sudo apt-get install libusb-dev
-sudo apt-get install libspnav-dev
-sudo apt-get install libbluetooth-dev
-sudo apt-get install libcwiid-dev
-
-#intallation du package de vision
-
-echo "Creating catkin Workspace for PR2 and MoveIt packages"
+echo "Creating catkin Workspace for PR2 and MoveIt packages in your home"
 cd ~
 git clone --recurse-submodules https://github.com/DavidTrimoulet/PR2-Kinetic-Xenial.git
 cd PR2-Kinetic-Xenial
 rosdep install --from-paths src/vision_visp/ --rosdistro kinetic
 rosdep install --from-paths src/ar_track_alvar/ --rosdistro kinetic
+
+echo "Installing dependencies"
+sudo apt -y install libfcl0.5 libfcl-0.5-dev scons libbson-dev libglew-dev libglew1.13 ros-kinetic-octomap ros-kinetic-octomap-mapping ros-kinetic-octomap-msgs ros-kinetic-octomap-ros ros-kinetic-octomap-rviz-plugins ros-kinetic-octomap-server ros-kinetic-geometric-shapes ros-kinetic-warehouse-ros ros-kinetic-srdfdom ros-kinetic-object-recognition-msgs ros-kinetic-navigation ros-kinetic-ivcon ros-kinetic-convex-decomposition python-pip
+
+sudo apt -y install ros-kinetic-pr2-common
+sudo apt -y install ros-kinetic-image-geometry
+sudo apt -y install ros-kinetic-cv-bridge
+sudo apt -y install ros-kinetic-perceptions-pctl
+sudo apt -y install ros-kinetic-gazebo-plugins
+sudo apt -y install ros-kinetic-ompl
+sudo apt -y install ros-kinetic-pr2-controllers-msgs
+sudo apt -y install ros-kinetic-rgbd-launch
+sudo apt -y install ros-kinetic-freenect-launch
+
+#gestion des contrler pour teleop
+sudo apt -y install libusb-dev
+sudo apt -y install libspnav-dev
+sudo apt -y install libbluetooth-dev
+sudo apt -y install libcwiid-dev
+
 catkin_make
 source ~/PR2-Kinetic-Xenial/devel/setup.bash
 
-echo "Creating catkin Workspace for MoveIt tutorial"
-mkdir -p ~/moveItTuto/src
-cd ~/moveItTuto/src
-git clone https://github.com/ros-planning/moveit_tutorials.git
-sudo pip install pyassimp
-cd ~/moveItTuto
-catkin_make
-source ~/moveItTuto/devel/setup.bash
+#intallation du package de vision
+
+
+
+#mkdir -p ~/moveItTuto/src
+#cd ~/moveItTuto/src
+#git clone https://github.com/ros-planning/moveit_tutorials.git
+#sudo pip install pyassimp
+#cd ~/moveItTuto
+#catkin_make
+#source ~/moveItTuto/devel/setup.bash
