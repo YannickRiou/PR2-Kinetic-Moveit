80943b476f6018fdccf49dbc6c9e7c82ea40bc56
David Trimoulet <davidt@hotmail.fr>
2018-07-09 16:38:06 +0200

    Maj sherlock

---
+++ src/robosherlock/cmake/robosherlock_config.cmake
@@ -0,0 +1,6 @@
+set(robosherlock_NAMESPACE           rs)
+set(robosherlock_TYPESYSTEM_CPP_PATH /home/dtrimoul/PR2-Kinetic-Xenial/src/robosherlock/src/core/include/rs/types)
+set(robosherlock_TYPESYSTEM_XML_PATH /home/dtrimoul/PR2-Kinetic-Xenial/src/robosherlock/descriptors/typesystem)
+set(robosherlock_ANNOTATOR_PATH      /home/dtrimoul/PR2-Kinetic-Xenial/src/robosherlock/descriptors/annotators)
+set(robosherlock_ENGINE_PATH         /home/dtrimoul/PR2-Kinetic-Xenial/src/robosherlock/descriptors/analysis_engines)
+
