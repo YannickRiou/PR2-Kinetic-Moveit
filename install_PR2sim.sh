b3b0a12360b240a24425a7429a7a3d9084023f2e
YannickRiou <59170639+YannickRiou@users.noreply.github.com>
2019-12-23 14:20:32 +0100

    Update install_PR2sim.sh
    Updated install script. Some install dependencies are maybe unnecessary.

--- install_PR2sim.sh
+++ install_PR2sim.sh
@@ -1,9 +1,12 @@
-#!/bin/bassh
+#!/bin/bash
 
 echo "Creating catkin Workspace for PR2 and MoveIt packages in your home"
 cd ~
-git clone --recurse-submodules https://github.com/DavidTrimoulet/PR2-Kinetic-Xenial.git
-cd PR2-Kinetic-Xenial
+mkdir ws_pr2
+cd ws_pr2
+git clone --branch master --recurse-submodules https://github.com/DavidTrimoulet/PR2-Kinetic-Xenial.git .
+mkdir -p src/vision_visp/
+mkdir -p src/ar_track_alvar/
 rosdep install --from-paths src/vision_visp/ --rosdistro kinetic
 rosdep install --from-paths src/ar_track_alvar/ --rosdistro kinetic
 
@@ -11,6 +14,7 @@ echo "Installing dependencies"
 sudo apt -y install libgtk2.0-dev libfcl0.5 libfcl-0.5-dev scons libbson-dev libglew-dev libglew1.13 ros-kinetic-octomap ros-kinetic-octomap-mapping ros-kinetic-octomap-msgs ros-kinetic-octomap-ros ros-kinetic-octomap-rviz-plugins ros-kinetic-octomap-server ros-kinetic-geometric-shapes ros-kinetic-warehouse-ros ros-kinetic-srdfdom ros-kinetic-object-recognition-msgs ros-kinetic-navigation ros-kinetic-ivcon ros-kinetic-convex-decomposition python-pip
 
 sudo apt -y install ros-kinetic-pr2-common
+sudo apt -y install ros-kinetic-pr2-controllers
 sudo apt -y install ros-kinetic-image-geometry
 sudo apt -y install ros-kinetic-cv-bridge
 sudo apt -y install ros-kinetic-perceptions-pctl
@@ -27,6 +31,7 @@ sudo apt -y install ros-kinetic-geometry
 sudo apt -y install ros-kinetic-tf2-geometry-msgs
 sudo apt -y install ros-kinetic-roslint
 sudo apt -y install ros-kinetic-filters
+
 sudo apt -y install flex ros-kinetic-mongodb-store ros-kinetic-tf2-bullet freeglut3-dev python-catkin-tools
 
 
@@ -37,16 +42,5 @@ sudo apt -y install libbluetooth-dev
 sudo apt -y install libcwiid-dev
 
 catkin_make
-source ~/PR2-Kinetic-Xenial/devel/setup.bash
-
-#intallation du package de vision
-
-
-
-#mkdir -p ~/moveItTuto/src
-#cd ~/moveItTuto/src
-#git clone https://github.com/ros-planning/moveit_tutorials.git
-#sudo pip install pyassimp
-#cd ~/moveItTuto
-#catkin_make
-#source ~/moveItTuto/devel/setup.bash
+source ~/ws_pr2/devel/setup.bash
+echo "source ~/ws_pr2/devel/setup.bash" >> ~/.bashrc 
